15d1169006e0a38513f2a3736090fcbf207125cc
zdebra <zd.brabec@gmail.com>
2022-03-12 22:44:55 +0100

    fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias gc "git commit -a"
+alias gp "git pull --rebase"
+alias gs "git status -s"
+alias ls "ls -GFh"
+alias vim "nvim"
+alias ll "exa -abghHliS"
